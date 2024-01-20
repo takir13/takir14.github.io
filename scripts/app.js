65260d0dd216d846f13cd86b61a7ef28fdc9fb68
Shahay13 <takirul.islam@dcmail.ca>
2024-01-20 01:40:20 -0500

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,97 @@
+"use strict";
+
+// IIFE: Immediately invoked functional expression
+(function(){
+    // Functions that run when the user is on that page
+    function DisplayHomePage() {
+        console.log("Called DisplayHomePage");
+
+        // Bind the button on the home page to a variable
+        let AboutUsButton = document.getElementById("AboutUsBtn");
+
+        // Assigning a variable with the first main tag on the homepage
+        let MainContent = document.getElementsByTagName("main")[0];
+
+        // Creating a paragraph tag inside the homepage
+        let MainParagraph = document.createElement("p");
+
+        // Set the id and class attribute to the paragraph tag
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+
+        // Add content to the paragraph
+        MainParagraph.textContent = "This is my first paragraph.";
+
+        // Attach Main paragraph to the main tag
+        MainContent.appendChild(MainParagraph);
+
+        let FirstString = "This is ";
+        let SecondString = `${FirstString}the main paragraph.`;
+
+        // Append the paragraph with the new string
+        MainParagraph.textContent = SecondString;
+
+        // Update the main tag
+        MainContent.appendChild(MainParagraph);
+
+        // Create a variable that accesses the body
+        let DocumentBody = document.body;
+
+        // Create an article tag
+        let Article = document.createElement("article");
+        let ArticleParagraph= `<p id = "ArticleParagraph" class = "mt-3"> This is my article paragraph</p>`;
+        Article.setAttribute("class", "container");
+
+        // Add the article attributes inside the article tag
+        Article.innerHTML = ArticleParagraph;
+
+        // Add the article content to the webpage
+        DocumentBody.appendChild(Article);
+
+        // Create an event listener for when the user clicks the button
+        // When the user clicks the button, redirect to the about us page
+        AboutUsButton.addEventListener("click", function (){
+            location.href = "about.html";
+        });
+    }
+
+    function DisplayAboutPage(){
+        console.log("Called DisplayAboutPage");
+    }
+
+    function DisplayContactPage(){
+        console.log("Called DisplayContactPage");
+    }
+
+    function DisplayProductPage(){
+        console.log("Called DisplayProductPage");
+    }
+
+    function DisplayServicesPage(){
+        console.log("Called DisplayServicesPage");
+    }
+
+    function Start(){
+        console.log("App Started...");
+
+        // Detect what page the user is on based on the HTML title tag
+        switch (document.title){
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "About":
+                DisplayAboutPage();
+                break;
+            case "Contact":
+                DisplayContactPage();
+                break;
+            case "Product":
+                DisplayProductPage();
+                break;
+            case "Services":
+                DisplayServicesPage();
+                break;
+        }
+    }
+    window.addEventListener("load", Start);
+})()
